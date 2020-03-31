6e1e9fbb53949a66bb0bdf013286a1a38bb8d38d
s16578 <s16578@pjwstk.edu.pl>
2020-03-31 21:20:26 +0200

    1st update

---
+++ cw_3/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw_3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+
+        public string GetStudent()
+        {
+            return "kamikaze";
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetStudents(int id)
+    {
+            if (id == 1)
+                return Ok("kowalski");
+            else if (id == 2)
+                return Ok("malczewski");
+            return NotFound("missing student");
+        
+    }
+
+    }
+}
