ddb959d23001112f5f45dbc24537f488ddc1ce5b
s16578 <s16578@pjwstk.edu.pl>
2020-03-31 23:06:16 +0200

    2nd commit

---
+++ cw_3/Model/TestInt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_3.Model
+{
+    public class TestInt
+    {
+        public int id { get; set; }
+    }
+}
