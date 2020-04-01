5b5a5e90ff108c6be8f2e32e892363ffaa395265
s16578 <s16578@pjwstk.edu.pl>
2020-04-01 21:41:36 +0200

    Added Delete/Put methods

--- cw_3/Controllers/StudentController.cs
+++ cw_3/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw_3.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,6 +30,27 @@ namespace cw_3.Controllers
             return NotFound("missing student");
         
     }
+        [HttpPost]
+
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 1000)}";
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"Student with {id} index has been removed");
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult InserStudent(int id)
+        {
+            return Ok($"Student with {id} idenx has been added");
+        }
 
     }
 }
