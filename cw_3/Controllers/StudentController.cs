73af7600318a3244b585ae8c0f5a24bb28cb00f9
s16578 <s16578@pjwstk.edu.pl>
2020-04-01 19:49:15 +0200

    Http GET/POST/SET/DELETE functions

--- cw_3/Controllers/StudentController.cs
+++ cw_3/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw_3.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,6 +30,26 @@ namespace cw_3.Controllers
             return NotFound("missing student");
         
     }
+        [HttpPost]
 
+        public IActionResult CreateStudent(Students student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 1000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult InsertStudent(int id)
+        {
+            return Ok($"student with {id} has been inserted");
+        }
+        
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"student with {id} had been removed");
+        }
     }
 }
