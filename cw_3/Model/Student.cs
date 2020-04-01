04987a928b01bef63fc07b03d8558c0ba8eb30d3
s16578 <s16578@pjwstk.edu.pl>
2020-04-01 21:23:09 +0200

    Student class added

---
+++ cw_3/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_3.Model
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LasteName { get; set; }
+        public string IndexNumber { get; set; }
+    }
+}
